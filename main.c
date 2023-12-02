93b2b17b9b47d8bb37d62592eadedee3f131af65
Muhammad Burhan Arshad <112951994+MuhammadBurhan123@users.noreply.github.com>
2023-12-02 17:23:34 +0500

    Update main.c

--- main.c
+++ main.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 
-void groud() {
+void drawGround(int pacman_x, int pacman_y) {
     int height = 20, width = 50;
 
-    for (int i = 1; i <= height ; i++) {
-        for (int j = 1; j <= width ; j++) {
+    for (int i = 1; i <= height; i++) {
+        for (int j = 1; j <= width; j++) {
             if (i == 1 || i == height || j == 1 || j == width) {
                 printf("*");
             } else {
-                if((i == 3 && j >= 2 && j <= 10) || (i == 3 && j >= 40 && j <= 49) || (i == 5 && j >= 15 && j <= 35) || (i == 7 && j >= 2 && j <= 20) || (i == 7 && j >= 30 && j <= 49) || (i == 9 && j >= 10 && j <= 40)){
+                if ((i == 3 && j >= 2 && j <= 10) || (i == 3 && j >= 40 && j <= 49) ||
+                    (i == 5 && j >= 15 && j <= 35) || (i == 7 && j >= 2 && j <= 20) ||
+                    (i == 7 && j >= 30 && j <= 49) || (i == 9 && j >= 10 && j <= 40)) {
                     printf("#");
-                }else if (i == 19 && j == 49){
+                } else if (i == pacman_y && j == pacman_x) {
                     printf("C");
-                }else{
+                } else {
                     printf(".");
                 }
             }
@@ -21,12 +23,50 @@ void groud() {
     }
 }
 
+void move(char direction, int *pacman_x, int *pacman_y) {
+    switch (direction) {
+        case 'w':
+            if (*pacman_y > 1) {
+                (*pacman_y)--;
+            }
+            break;
+        case 's':
+            if (*pacman_y < 19) {
+                (*pacman_y)++;
+            }
+            break;
+        case 'a':
+            if (*pacman_x > 2) { 
+                (*pacman_x)--;
+            }
+            break;
+        case 'd':
+            if (*pacman_x < 49) {
+                (*pacman_x)++;
+            }
+            break;
+        default:
+            break;
+    }
+}
+
 int main() {
+    int pacman_y = 19, pacman_x = 49;
 
-    char action;
+    char key;
+    do {
+        printf("\e[1;1H\e[2J");
 
-    groud();
+        drawGround(pacman_x, pacman_y);
 
-    scanf("%c", action);
+        printf("Press a key (w/a/s/d) or press Esc to exit: ");
+        scanf(" %c", &key);
+
+        if (key != 27) { 
+            move(key, &pacman_x, &pacman_y);
+
+        }
+    } while (key != 27);
+
     return 0;
 }
